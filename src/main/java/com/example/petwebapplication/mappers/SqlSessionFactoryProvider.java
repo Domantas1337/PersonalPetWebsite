f8096e66ed6de33e0fbff6235a1e04de043b8f25
Domantas1337 <domantas.kakc@gmail.com>
2024-03-04 22:20:08 +0200

    Getting ready for using MyBatis

---
+++ src/main/java/com/example/petwebapplication/mappers/SqlSessionFactoryProvider.java
@@ -0,0 +1,27 @@
+package com.example.petwebapplication.mappers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+@RequestScoped
+public class SqlSessionFactoryProvider {
+
+    @Produces
+    @ApplicationScoped
+    public SqlSessionFactory produceFactory() {
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            return new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
